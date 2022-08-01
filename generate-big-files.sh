313e57cf2b679ad1ff8ba6772210268a33d68bda
Marat Reymers <marat.maratori@gmail.com>
2022-08-02 01:06:35 +0300

    Generator

---
+++ generate-big-files.sh
@@ -0,0 +1,7 @@
+openssl rand -hex -out big-file-1.txt $((512*1024*40))
+openssl rand -hex -out big-file-2.txt $((512*1024*40))
+openssl rand -hex -out big-file-3.txt $((512*1024*40))
+openssl rand -hex -out big-file-4.txt $((512*1024*40))
+openssl rand -hex -out big-file-5.txt $((512*1024*40))
+openssl rand -hex -out big-file-6.txt $((512*1024*40))
+
